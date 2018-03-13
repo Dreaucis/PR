7c03e1778a8c6206ecd975d4a7f09c787c4f9046
Xavier <savvesd@gmail.com>
2018-03-13 21:37:40 +0100

    Implemented a basic mouse tracking calss, which returns coordinates from drawings

--- track_mouse.py
+++ track_mouse.py
@@ -1,38 +1,67 @@
-from pynput import mouse
+import numpy as np
+import matplotlib.pyplot as plt
 
-class IsPressed:
-    def __init__(self):
-        self.boolean = False
-    def setFalse(self):
-        self.boolean = False
-    def setTrue(self):
-        self.boolean = True
-    def get(self):
-        return self.boolean
+class MouseDragTracker:
+    def __init__(self, line):
+        self.line = line
+        self.xs = list(line.get_xdata())
+        self.ys = list(line.get_ydata())
+        self.isPressed = False
 
-isPressed = IsPressed()
-myList = list()
-def on_move(x, y):
-    if isPressed.get():
-        myList.append((x, y))
+    def connect(self):
+        'connect to all the events we need'
+        self.cidpress = self.line.figure.canvas.mpl_connect(
+            'button_press_event', self.on_press)
+        self.cidrelease = self.line.figure.canvas.mpl_connect(
+            'button_release_event', self.on_release)
+        self.cidmotion = self.line.figure.canvas.mpl_connect(
+            'motion_notify_event', self.on_motion)
+        self.cidend = self.line.figure.canvas.mpl_connect('key_press_event', self.on_key)
 
-def on_click(x, y, button, pressed):
-    print('{0} at {1}'.format(
-        'Pressed' if pressed else 'Released',
-        (x, y)))
-    if pressed:
-        isPressed.setTrue()
-    else:
-        isPressed.setFalse()
+    def on_press(self, event):
+        'on button press we will see if the mouse is over us and store some data'
+        if event.inaxes != self.line.axes: return
+        self.isPressed = True
 
-def on_scroll(x, y, dx, dy):
-    return False
+    def on_key(self,event):
+        print('you pressed', event.key, event.xdata, event.ydata)
+        self.disconnect()
 
-# Collect events until released
-with mouse.Listener(
-        on_move=on_move,
-        on_click=on_click,
-        on_scroll=on_scroll) as listener:
-    listener.join()
 
-print(myList)
+    def on_motion(self, event):
+        'on motion we will move the rect if the mouse is over us'
+        if not self.isPressed: return
+        if event.inaxes != self.line.axes: return
+
+        self.xs.append(event.xdata)
+        self.ys.append(event.ydata)
+        self.line.set_data(self.xs, self.ys)
+        self.line.figure.canvas.draw()
+
+    def on_release(self, event):
+        'on release we reset the press data'
+        self.isPressed = False
+        self.line.figure.canvas.draw()
+
+    def disconnect(self):
+        'disconnect all the stored connection ids'
+        self.line.figure.canvas.mpl_disconnect(self.cidpress)
+        self.line.figure.canvas.mpl_disconnect(self.cidrelease)
+        self.line.figure.canvas.mpl_disconnect(self.cidmotion)
+
+    def get_crds(self):
+        return (self.xs,self.ys)
+
+class MDTPlotHandler:
+    def run(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        rect, = ax.plot([], [],'*')
+        mdt = MouseDragTracker(rect)
+        mdt.connect()
+        plt.show()
+        draw_crds = np.matrix(mdt.get_crds())
+        return draw_crds
+
+
+a = MDTPlotHandler().run()
