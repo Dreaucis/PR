53391185bef0aae0c1d1437df7691196c351331f
Xavier <savvesd@gmail.com>
2018-03-18 15:03:01 +0100

    included testin file

--- testingFeatures.py
+++ testingFeatures.py
@@ -1,12 +1,9 @@
-print(1)
-#import matplotlib.pyplot as plt
-#from MDTPlotHandler import MDTPlotHandler
-#from feature_extractor import FeatureExtractor
+import matplotlib.pyplot as plt
+from MDTPlotHandler import MDTPlotHandler
+from feature_extractor import FeatureExtractor
 
-print(1)
 a = MDTPlotHandler().run()
 b = FeatureExtractor(a)
 c = b.dist2cent()
-print(c)
 plt.plot(c)
 plt.show()
