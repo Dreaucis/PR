b2c90d3c40e150479db38191ea54bdff32958635
Xavier <savvesd@gmail.com>
2018-03-27 22:51:49 +0200

    implemented backward, also gitignore

--- HMM/hidden_markov_chain.py
+++ HMM/hidden_markov_chain.py
@@ -1,6 +1,6 @@
 import numpy as np
 from distributions.GaussD.gaussian_dist import GaussianDist,logProb
-from MC.markov_chain import forward, FiniteMarkovChain
+from MC.markov_chain import forward, FiniteMarkovChain, backward
 class HiddenMarkovChain:
     def __init__(self,mc,emissionDist):
         self.mc = mc
@@ -18,6 +18,8 @@ class HiddenMarkovChain:
         pX,_ = logScale(pX)
         _,c = forward(self.mc,pX)
         print(c)
+        betaHat = backward(self.mc,pX,c)
+        print(betaHat)
         logP = sum(np.log(c))
         return logP
 
@@ -31,7 +33,7 @@ def logScale(probMat):
 initProb = np.matrix('1;0')
 transProb = np.matrix('0.9,0.1,0;0,0.9,0.1')
 x = np.matrix('1,0.8,0.7,0.6,0.5,0.0,0.0,0.0,0.0,0.2,0.3,0.4,0.5,0.9,0.7,0.2,0.0,0.0,0.0,0.0,0.0,0.1,0.3,0.8')
-fmc = FiniteMarkovChain(initProb,transProb)
+fmc = FiniteMarkovChain(initProb, transProb)
 gD1 = GaussianDist(0,1)
 gD2 = GaussianDist(3,2)
 hmm = HiddenMarkovChain(fmc,[gD1,gD2])
--- MC/markov_chain.py
+++ MC/markov_chain.py
@@ -3,6 +3,8 @@ class FiniteMarkovChain():
     def __init__(self,initProb,transProb):
         self.initProb = initProb
         self.transProb = transProb
+        self.isFinite = True
+
     def nStates(self):
         return np.size(self.transProb,0)
     def setInitProb(self,initProb):
@@ -16,7 +18,7 @@ class FiniteMarkovChain():
 
 def forward(fmc,pX):
     """
-    :param fmc: FiniteMarkovChain 
+    :param fmc: FiniteMarkovChain
     :param pX: numpy matriix with entries  pX(j,t) = P(x_t|s_t = j). 
     Probability of observation given state.
     :return: alfahat, c
@@ -44,9 +46,25 @@ def forward(fmc,pX):
 
     return alfaHat,c
 
+def backward(fmc,pX,c):
+    T = np.size(pX, 1)
+    nS = fmc.nStates()
+    A = fmc.getTransProb()
+    betaHat = np.zeros((nS,T))
+    for i in range(0,nS):
+        betaHat[i,T-1] = A[i,nS]/(c[0,T-1]*c[0,T])
+    for t in range(T-2,-1,-1):
+        for i in range(0,nS):
+            summ = 0
+            for j in range(0,nS):
+                summ = summ + A[i,j]*pX[j,t+1]*betaHat[j,t+1]
+            betaHat[i,t] = summ/c[0,t]
+    return betaHat
+
 def test():
     initProb = np.matrix('1;0;0')
     transProb = np.matrix('0.5,0.5,0,0;0,0.5,0.5,0;0,0,0.5,0.5')
     pX = np.matrix('1,0.8,0.7,0.6,0.5,0.0,0.0,0.0;0.0,0.2,0.3,0.4,0.5,0.9,0.7,0.2;0.0,0.0,0.0,0.0,0.0,0.1,0.3,0.8')
-    fmc = FiniteMarkovChain(initProb,transProb)
+    fmc = FiniteMarkovChain(initProb, transProb)
     alfahat,c = forward(fmc,pX)
+
