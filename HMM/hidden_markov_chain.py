Implemented wrapped Gaussian, fixed angle calculation

HMM/hidden_markov_chain.py
@@ -1,6 +1,6 @@
 import numpy as np
 import numpy.matlib
-from distributions.GaussD.gaussian_dist import GaussianDist,logProb
+from distributions.GaussD.gaussian_dist import GaussianDist
 from MC.markov_chain import forward, FiniteMarkovChain, backward
 
 
@@ -28,7 +28,7 @@ class HiddenMarkovChain:
         logP = np.sum(np.log(c))
         return logP
 
-def MakeLeftRightHMM(nStates,obsData,lData = []):
+def MakeLeftRightHMM(nStates,pD,obsData,lData = []):
     """
     :param nStates:
     :param pD:
@@ -38,9 +38,9 @@ def MakeLeftRightHMM(nStates,obsData,lData = []):
     """
     if len(lData) == 0:
         lData = [np.size(obsData,1)]
-    pD = []
-    for i in range(0, nStates):
-        pD.append(GaussianDist())
+    #pD = []
+    #for i in range(0, nStates):
+    #    pD.append(GaussianDist())
     D = np.mean(lData)
     D = D/nStates
     mc = FiniteMarkovChain(); initLeftRight(mc,nStates,D)

distributions/GaussD/gaussian_dist.py
@@ -1,6 +1,6 @@
-from numpy.random import multivariate_normal
 import numpy as np
 from numpy import random
+from numpy import linalg as LA
 
 
 class GaussianDist:
@@ -22,9 +22,23 @@ class GaussianDist:
         self.dataSize = dataSize
     def generate(self):
         random.normal(self.mean,self.stDev,self.dataSize)
-    def prob(self,x):
+    def prob(self,x): #TODO: THIS DOES NOT GIVE MULTIVARIATE ESTIMATES CORRECTLY!!!
         x = np.asmatrix(x)
-        return np.transpose((1/(np.sqrt(2*np.pi*np.square(self.stDev)))))*np.exp(-np.square(x-self.mean)/(2*np.square(self.stDev)))
+        pdf = 1# np.transpose((1/(np.sqrt(2*np.pi*np.square(self.stDev)))))*np.exp(-np.square(x-self.mean)/(2*np.square(self.stDev)))
+        covMatrix = np.asmatrix(np.diagflat(np.square(self.stDev)))
+        #print(np.asmatrix(covMatrix).shape[0])
+
+        dim = covMatrix.shape[0]
+        pdf_2 = np.exp(-(1/2)*(x-self.mean).T*covMatrix.I*(x-self.mean)) / np.sqrt((2*np.pi)**dim * LA.det(covMatrix))
+        #print((x-self.mean))
+        #print((x-self.mean).T)
+        #print('fdsfsdfs')
+        #print(covMatrix.I)
+        #print(-(1/2)*(x-self.mean).T*covMatrix.I*(x-self.mean))
+        #print(pdf)
+        #print(pdf_2)
+
+        return pdf
     # TODO: Put in LogScale in above!!!!!
 def logProb(pD,x):
     nObj = 1
@@ -48,4 +62,9 @@ def logProb(pD,x):
         else:
             print('ERROR')
     return logP
+
+def testGaussianDist():
+    a = GaussianDist(np.asmatrix('1;1'),np.asmatrix('1;1'))
+    a.prob(np.asmatrix('1,2,3;1,1,1'))
+testGaussianDist()
 #TODO: Fix so that it can take a vecotr of distributions as input!

feature_extracting/feature_extractor.py
@@ -63,5 +63,9 @@ class FeatureExtractor:
         return euclDist
 
     def angleOfMotion(self):
-        angleOfMotion = np.arctan2(self.normCords[0,:],self.normCords[1,:])
+        #angleOfMotion = np.arctan2(self.normCords[0,:],self.normCords[1,:])
+        diffCords = np.diff(self.cords)
+        diffCordsExtended = np.concatenate((diffCords[:,0],diffCords),1)
+
+        angleOfMotion = np.arctan2(diffCordsExtended[0, :], diffCordsExtended[1, :])
         return angleOfMotion